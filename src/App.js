fd81f3497cd4c0de21c34862757e033415d7e9ab
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-12 17:00:28 +0700

    achieved sending data to firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const isVisible = useSelector((state) => state.ui.isVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-2d778-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {isVisible && <Cart />}
