82a9c01b7a2bc1e0e1d3e3974282b4a02a0850a2
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-06-12 10:06:40 +0700

    added quantity and items function

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,11 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
+import { cartAction } from "../../store/cartSlice";
+import { useCallback } from "react";
 
 const ProductItem = (props) => {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.items);
   const { title, price, description } = props;
 
-  const addToCartHandler = () => {
-  };
+  const addToCartHandler = useCallback(
+    (title, price) => {
+      const existingItem = items.find((item) => item.title === title);
+
+      if (existingItem) {
+        dispatch(cartAction.addQuantity(title));
+      } else {
+        dispatch(cartAction.addToCart({ title, quantity: 1, price }));
+      }
+      console.log(items);
+    },
+    [dispatch, items]
+  );
 
   return (
     <li className={classes.item}>
@@ -16,7 +32,9 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button onClick={addToCartHandler}>Add to Cart</button>
+          <button onClick={() => addToCartHandler(title, price)}>
+            Add to Cart
+          </button>
         </div>
       </Card>
     </li>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -2,8 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: [],
-  reducers: {},
+  initialState: { items: [] },
+  reducers: {
+    addToCart(state, action) {
+      state.items.push(action.payload);
+    },
+    addQuantity(state, action) {
+      const existingItem = state.items.find(
+        (item) => item.title === action.payload
+      );
+      if (existingItem) {
+        existingItem.quantity++;
+      }
+    },
+  },
 });
 
 export const cartAction = cartSlice.actions;
